0fd6a04a5a005e2810254a43515daf8315f78480
disp1air <hola-shola@mail.ru>
2018-01-27 17:02:39 +0300

    add simple module

--- Chapter_3/script1.py
+++ Chapter_3/script1.py
@@ -1,6 +1,10 @@
 #First script on Python lang
 import sys              #load library module
+import myfile
+
 print(sys.platform)
+print(myfile.title)
+
 print(2 ** 100)
 x = 'Spam'
 print(x * 8)            #repeat string
