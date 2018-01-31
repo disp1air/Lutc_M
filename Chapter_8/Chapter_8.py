182f7a98a31553fb486d05d09f03a0d915d5f5bf
disp1air <hola-shola@mail.ru>
2018-01-31 21:33:07 +0300

    for in dict

--- Chapter_8/Chapter_8.py
+++ Chapter_8/Chapter_8.py
@@ -6,3 +6,12 @@ print(D)
 print(len(D))
 print(D.keys())
 
+D['ham'] = ['grill', 'bake', 'fry']
+print(D)
+# D = {'spam': 1, 'ham': ['grill', 'bake', 'fry'], 'eggs': 4}
+
+del D['eggs']   # удаление элемента
+print(D)
+
+D['brunch'] = 'Bacon'   # добавление элемента
+print(D)
