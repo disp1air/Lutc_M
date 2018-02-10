eff79232e02b283222e35852f5879a7e94ed0efb
disp1air <hola-shola@mail.ru>
2018-02-10 13:24:55 +0300

    add classtools

--- Chapter_27/person.py
+++ Chapter_27/person.py
@@ -12,9 +12,12 @@ class Person:
         self.pay = int(self.pay * (percent + 1))
 
     def __str__(self):
-        return '[Person: %s, %s]' % (self.name, self.pay)
+        return '[Person: %s, %s, %s]' % (self.name, self.job, self.pay)
 
 class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'mnrg', pay)
+
     def giveRaise(self, percent, bonus = .10):
         Person.giveRaise(self, percent + bonus)
     #    self.pay = int(self.pay * (1 + percent + bonus))
@@ -31,7 +34,12 @@ if __name__ == '__main__':   # когда файл запускается(сам
     print(sue.pay)
     print(sue)
 
-    tom = Manager('Tom Jones', 'mngr', 50000)
+    tom = Manager('Tom Jones', 50000)
     tom.giveRaise(.10)
     print(tom.lastName())
     print(tom)
+
+    print('--All Three--')
+    for object in (bob, sue, tom):
+        object.giveRaise(.10)
+        print(object)
