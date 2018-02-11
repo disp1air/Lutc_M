f7ddedf6f69d14c4b2e0bdd8beda9742c9153565
disp1air <hola-shola@mail.ru>
2018-02-11 14:49:59 +0300

    add new info about dict(creation examples)

--- Chapter_19/mapExample.py
+++ Chapter_19/mapExample.py
@@ -3,6 +3,9 @@ counters = [1, 2, 3, 4, 5]
 def inc(x):
     return x + 10
 
+# в версии 3.х функция map возвращает генератор
+# поэтому приведем результат в удобный вид, т.е. к списку
+
 print(list(map(inc, counters)))
 
 print(list(map((lambda x: x + 100), counters)))
