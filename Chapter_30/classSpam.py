2d27c9a8d4c8272118c90068ee0e64aa8ea020db
disp1air <hola-shola@mail.ru>
2018-02-13 20:25:25 +0300

    add lister and other

---
+++ Chapter_30/classSpam.py
@@ -0,0 +1,8 @@
+from lister import ListInstance
+
+class Spam(ListInstance):
+    def __init__(self):
+        self.data1 = 'food'
+
+x = Spam()
+print(x) 
---
+++ Chapter_30/outputMixins.py
@@ -0,0 +1,7 @@
+import lister
+
+class C(lister.ListInstance): pass
+
+x = C()
+x.a = 1; x.b = 2; x.c = 3
+print(x)
